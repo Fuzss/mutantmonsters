7cdeab8858261ec27481c74db577900c1d95f967
Fuzss <28218241+Fuzss@users.noreply.github.com>
2023-11-30 13:41:54 +0100

    fix cardinal components api

--- 1.20/Common/src/main/java/fuzs/mutantmonsters/proxy/Proxy.java
+++ 1.20/Common/src/main/java/fuzs/mutantmonsters/proxy/Proxy.java
@@ -1,11 +1,10 @@
 package fuzs.mutantmonsters.proxy;
 
 import fuzs.mutantmonsters.world.entity.CreeperMinion;
-import fuzs.puzzleslib.api.core.v1.DistTypeExecutor;
+import fuzs.puzzleslib.api.core.v1.ModLoaderEnvironment;
 
 public interface Proxy {
-    @SuppressWarnings("Convert2MethodRef")
-    Proxy INSTANCE = DistTypeExecutor.getForDistType(() -> () -> new ClientProxy(), () -> () -> new ServerProxy());
+    Proxy INSTANCE = ModLoaderEnvironment.INSTANCE.isClient() ? new ClientProxy() : new ServerProxy();
 
     void displayCreeperMinionTrackerGUI(CreeperMinion creeperMinion);
 
