fcbfc1cc9f554ce1722f15292bbbbdc0097c4010
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-02-13 20:45:56 +0100

    migrate skeleton armor to attributes

--- 1.21.4/Common/src/main/java/fuzs/mutantmonsters/world/item/SkeletonArmorItem.java
+++ 1.21.4/Common/src/main/java/fuzs/mutantmonsters/world/item/SkeletonArmorItem.java
@@ -2,29 +2,56 @@ package fuzs.mutantmonsters.world.item;
 
 import fuzs.puzzleslib.api.core.v1.Proxy;
 import net.minecraft.ChatFormatting;
-import net.minecraft.core.Holder;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ArmorItem;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlotGroup;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.item.equipment.ArmorMaterial;
 import net.minecraft.world.item.equipment.ArmorType;
-import net.minecraft.world.level.Level;
 
 import java.util.List;
 
-public class SkeletonArmorItem extends ArmorItem {
-    private final ArmorType armorType;
+public class SkeletonArmorItem extends Item {
 
-    public SkeletonArmorItem(ArmorMaterial material, ArmorType armorType, Properties properties) {
-        super(material, armorType, properties);
-        this.armorType = armorType;
+    public SkeletonArmorItem(ArmorMaterial armorMaterial, ArmorType armorType, Properties properties) {
+        super(skeletonProperties(armorMaterial, armorType, properties));
+    }
+
+    static Item.Properties skeletonProperties(ArmorMaterial armorMaterial, ArmorType armorType, Properties properties) {
+        armorMaterial.humanoidProperties(properties, armorType);
+        switch (armorType) {
+            case BOOTS ->
+                    properties.attributes(withJumpStrength(armorType, armorMaterial.createAttributes(armorType), 2));
+            case LEGGINGS ->
+                    properties.attributes(withMovementSpeed(armorType, armorMaterial.createAttributes(armorType), 2));
+        }
+        return properties;
+    }
+
+    static ItemAttributeModifiers withJumpStrength(ArmorType armorType, ItemAttributeModifiers itemAttributeModifiers, int amplifier) {
+        EquipmentSlotGroup equipmentSlotGroup = EquipmentSlotGroup.bySlot(armorType.getSlot());
+        ResourceLocation resourceLocation = ResourceLocation.withDefaultNamespace("armor." + armorType.getName());
+        return itemAttributeModifiers.withModifierAdded(Attributes.JUMP_STRENGTH,
+                        new AttributeModifier(resourceLocation, 0.1 * amplifier, AttributeModifier.Operation.ADD_VALUE),
+                        equipmentSlotGroup)
+                .withModifierAdded(Attributes.SAFE_FALL_DISTANCE,
+                        new AttributeModifier(resourceLocation, 1.0 * amplifier, AttributeModifier.Operation.ADD_VALUE),
+                        equipmentSlotGroup);
+    }
+
+    static ItemAttributeModifiers withMovementSpeed(ArmorType armorType, ItemAttributeModifiers itemAttributeModifiers, int amplifier) {
+        EquipmentSlotGroup equipmentSlotGroup = EquipmentSlotGroup.bySlot(armorType.getSlot());
+        ResourceLocation resourceLocation = ResourceLocation.withDefaultNamespace("armor." + armorType.getName());
+        return itemAttributeModifiers.withModifierAdded(Attributes.MOVEMENT_SPEED,
+                new AttributeModifier(resourceLocation,
+                        0.2 * amplifier,
+                        AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL),
+                equipmentSlotGroup);
     }
 
     @Override
@@ -34,21 +61,5 @@ public class SkeletonArmorItem extends ArmorItem {
 
     public Component getDescriptionComponent() {
         return Component.translatable(this.getDescriptionId() + ".description").withStyle(ChatFormatting.GOLD);
-    }
-
-    @Override
-    public void inventoryTick(ItemStack itemStack, Level level, Entity entity, int slotId, boolean isSelected) {
-        if (entity instanceof Player player && slotId == this.armorType.getSlot().getIndex()) {
-            if (player.getInventory().getArmor(slotId) == itemStack) {
-                Holder<MobEffect> mobEffect = switch (this.armorType) {
-                    case BOOTS -> MobEffects.JUMP;
-                    case LEGGINGS -> MobEffects.MOVEMENT_SPEED;
-                    default -> null;
-                };
-                if (mobEffect != null) {
-                    player.addEffect(new MobEffectInstance(mobEffect, 100, 1, false, false));
-                }
-            }
-        }
     }
 }
